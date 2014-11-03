954e562241aa83c9d85905a21f03db9abfb1633e
danabrain <dana.breen@gmail.com>
2014-11-03 08:50:32 -0500

    save images

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
-$('document').ready() {
-  alert("HI");
-}
+$(document).ready(function() {
+  $("#yes").click(function() {
+    $(".quizbox").css("display", "none");
+    $(".question").show();
+  });
+
+  $("#no").click(function() {
+    $(".quizbox").css("display", "none");
+    $(".unicorn").css("display", "none");
+    $(".noquiz").show();
+  });
+
+  $("#maybe").click(function() {
+    $(".quizbox").css("display", "none");
+    $(".unicorn").show();
+  })
+
+  $("#restart").click(function() {
+    $(".noquiz").css("display", "none");
+    $(".quizbox").show();
+  });
+
+  $(".navbar").click(function() {
+    $(".noquiz").css("display", "none");
+    $(".question").css("display", "none");
+    $(".unicorn").css("display", "none");
+    $(".quizbox").show();
+  });
+
+});
