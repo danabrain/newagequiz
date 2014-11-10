a51be33b6bc5936587b34be39d82c4d3ae2029f7
danabrain <dana.breen@gmail.com>
2014-11-10 08:07:40 -0500

    update

--- app.js
+++ app.js
@@ -1,30 +1,96 @@
-$(document).ready(function() {
-  $("#yes").click(function() {
-    $(".quizbox").css("display", "none");
-    $(".question").show();
-  });
+function chooseGame() {
+    $("#yes").click(function() {
+      $(".quizbox").css("display", "none");
+     $(".question").show();
+    });
 
-  $("#no").click(function() {
-    $(".quizbox").css("display", "none");
-    $(".unicorn").css("display", "none");
-    $(".noquiz").show();
-  });
+    $("#no").click(function() {
+      $(".quizbox").css("display", "none");
+      $(".unicorn").css("display", "none");
+      $(".noquiz").show();
+    });
 
-  $("#maybe").click(function() {
-    $(".quizbox").css("display", "none");
-    $(".unicorn").show();
-  })
+    $("#maybe").click(function() {
+      $(".quizbox").css("display", "none");
+      $("#blankheart").show();
+      $("#gettingthere").css("display", "none");
+      $("#shining").css("display", "none");
+      $(".unicorn").show();
+    });
+  };
 
+  function startOver() {
   $("#restart").click(function() {
     $(".noquiz").css("display", "none");
-    $(".quizbox").show();
+    $(".question2").css("display", "none");
+    $(".question").show();
   });
 
   $(".navbar").click(function() {
     $(".noquiz").css("display", "none");
     $(".question").css("display", "none");
     $(".unicorn").css("display", "none");
+    $(".question2").css("display", "none");
     $(".quizbox").show();
   });
+};
+
+function shineHeart() {
+  $("#blankheart").click(function() {
+    $("#blankheart").css("display", "none");
+    $("#gettingthere").show();
+  });
+
+  $("#gettingthere").click(function() {
+    $("#blankheart").css("display", "none");
+     $("#gettingthere").css("display", "none");
+     $("#shining").show();
+  });
+
+  $("#shining").click(function() {
+    $(".unicorn").css("display", "none");
+    $(".quizbox").css("display", "none");
+    $(".question").show();
+  });
+};
+
+ var answerArrayOne = [];
+  var answerArrayTwo = [];
+  var answerArrayThree = [];
+
+$(document).ready(function() {
+
+  var test = {question:"Why?", one:"Yes", two:"No"};
+
+  document.getElementById("demo");
+
+  chooseGame();
+
+  startOver();
+
+  shineHeart();
+
+  $("#one").click(function() {
+    answerArrayOne.push(1);
+    console.log(answerArrayOne);
+    $(".question").css("display", "none");
+    $(".question2").show();
+  })
+
+  $("#two").click(function() {
+    answerArrayTwo.push(1);
+    console.log(answerArrayTwo);
+    $(".question").css("display", "none");
+    $(".question2").show();
+  })
+
+  $("#three").click(function() {
+    answerArrayThree.push(1);
+    console.log(answerArrayThree);
+    $(".question").css("display", "none");
+    $(".question2").show();
+  })
+
+
 
 });
